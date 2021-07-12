df37c79b0bf81d5e5ef853308e4b55772fae3575
nmammana <nicomammana14@gmail.com>
2021-07-12 10:40:33 -0300

    Primera correccion. Pagina terminada

--- src/components/ProjectCard.js
+++ src/components/ProjectCard.js
@@ -7,16 +7,7 @@ import './styles/ProjectCard.css';
 export default function ProjectCard(props) {
     const {projectImg, title, description, buttonFirst, buttonSecond, profileImg, comment, client, imageFirst} = props;
 
-    useEffect(()=>{
-        /*var element = document.getElementById('project');
-        element.className += " imageFirst";
-                
-        var elemento = document.getElementsByClassName("project");
-        for(var i = 0; i < elemento.length; i++){
-            elemento[i].style.background = 'red';
-            elemento[i].className += "imageFirst";
-        }*/
-        
+    useEffect(()=>{        
         var projectPic = document.getElementsByClassName('project_pic');
         var projectPresentation = document.getElementsByClassName('project_presentation');
         var projectImg = document.getElementsByClassName('projectImg');
@@ -27,17 +18,6 @@ export default function ProjectCard(props) {
         projectPresentation[1].style['grid-row'] = '1';
         
         projectImg[2].style['width'] = '15em';
-       /* for(var i=0; i<projectPic.length; i++){
-            
-        }
-        for(var j=0; j<projectPresentation.length; j++){
-            
-        }*/
-        
-        
-        /*if({imageFirst}){          
-            element.style.background = 'red';
-        }*/
     })
 
     return (
